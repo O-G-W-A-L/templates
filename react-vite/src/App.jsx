da720b9d33733a9e6c9197b53b44bf4d99be450d
O-G-W-A-L <ogwalja04@gmail.com>
2025-01-02 14:45:26 +0300

    template

---
+++ react-vite/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+///import './App.css'
+import './index.css';
+
+
+function App() {
+
+  return (
+    <>
+    <h1
+    className="text-8xl text-center text-blue-900" 
+    >Welcome to my vite+react+tailwind template</h1>
+    </>
+  )
+}
+
+export default App
